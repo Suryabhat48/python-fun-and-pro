7bb71f477a96e521b46047daf2a9cf1705240012
Suryabhat48 <suryabhat2032@gmail.com>
2025-08-08 07:14:44 +0530

    Add two files

---
+++ fun-rec.py
@@ -0,0 +1,23 @@
+# #Function definition 
+# def sum(a,b):#Functional parameters
+#     sum=a+b
+#     print(sum)
+#     return sum
+
+
+# sum(2,5)#function call and the arguments passed
+# def hello():
+#     print("Hello world")
+# hello()
+# hello()
+# hello()
+# hello()
+# def avg(a,b):
+#     return (a+b)/2
+# print(avg(2,2))
+
+
+nums=[1,2,3,4,5,6,7,8,9,10]
+def length(a):
+    print(len(a))
+length(nums)
